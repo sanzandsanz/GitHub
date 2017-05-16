525beadbf83f2b81a26a8bce41a4293f14d1f6b9
sanzandsanz <sanjay.bajracharya4@gmail.com>
2017-05-16 11:59:05 +1000

    My first commit on github

---
+++ FirstGitHubConsoleApp/FirstGitHubConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGitHubConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first github test");
+        }
+    }
+}
